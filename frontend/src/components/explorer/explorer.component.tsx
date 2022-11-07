1673e4ba8abcf31ef9fe466fb782f2324e95867d
Oleksandr <portianko123@gmail.com>
2022-11-07 19:34:32 +0200

    updated files structure

--- frontend/src/components/explorer/explorer.component.tsx
+++ frontend/src/components/explorer/explorer.component.tsx
@@ -6,27 +6,83 @@ import fileSvg from '../../assets/file.svg'
 import './explorer.styles.css'
 
 const Explorer = () => {
-    const [path, setPath] = useState(['../', 'main'])
+    const [path, setPath] = useState<string[]>(['../'])
+    const structure = [
+        {
+            path: '../',
+            type: 'file',
+            name: 'text',
+        },
+        {
+            path: '../',
+            type: 'folder',
+            name: 'folder1',
+            folder_path: 'folder1',
+            children: [
+                {
+                    path: 'folder1',
+                    type: 'file',
+                    name: 'textNew',
+                }
+            ]
+        },
+        {
+            path: '../',
+            type: 'file',
+            name: 'folder2',
+        },
+        {
+            path: '../',
+            type: 'file',
+            name: 'text2',
+        }
+    ]
+
+    const handleFolderClick = (folderPath: string | undefined) => {
+        if (folderPath) {
+            const newPath = [...path, folderPath]
+            setPath(newPath)
+        }
+    }
+
+    const handleFileClick = () => {
+        console.log('file click')
+    }
 
     return (
         <div className='explorer-container'>
             <h4 className='explorer-title'>My files</h4>
             <div className='explorer-path-block'>
-                { 
-                    path.map((el) => (
-                        <span className='explorer-path-item'>{el}</span>
+                {
+                    path.map((el, idx) => (
+                        <span key={el + idx} onClick={handle} className='explorer-path-item'>{el}</span>
                     ))
                 }
             </div>
             <div className='folders-structure'>
-                <div className='folder-item-block'>
-                    <img className='folder-item-icon' src={folderSvg} alt="" />
-                    <span className='folder-item-name'>Folder name</span>
-                </div>
-                <div className='folder-item-block'>
-                    <img className='folder-item-icon' src={fileSvg} alt="" />
-                    <span className='folder-item-name'>File name</span>
-                </div>
+                {
+                    structure.map((file) => {
+                        if (file.path === path.slice(-1)[0]) {
+                            if (file.type === 'folder') {
+                                return (
+                                    <div className='folder-item-block' onDoubleClick={() => handleFolderClick(file.folder_path)}>
+                                        <img className='folder-item-icon' src={folderSvg} alt="" />
+                                        <span className='folder-item-name'>{file.name}</span>
+                                    </div>
+                                )
+                            } else {
+                                return (
+                                    <div className='folder-item-block' onDoubleClick={handleFileClick}>
+                                        <img className='folder-item-icon' src={fileSvg} alt="" />
+                                        <span className='folder-item-name'>{file.name}</span>
+                                    </div>
+                                )
+                            }
+                        } else {
+                            return <></>
+                        }
+                    })
+                }
             </div>
         </div>
     )
